ced8c88cccbaa7c0bc6892f939d83daad99e75a8
46766 <46766@DESKTOP-P0JDUG9>
2022-10-03 01:28:40 +0200

    Add project files.

---
+++ CalculatorV2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CalculatorV2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
